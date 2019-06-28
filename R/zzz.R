960132e808325a2a863a51b94efb262e10b51111
wilsontom <wilsontom89@gmail.com>
2019-06-28 14:06:14 +0100

    add global variables

---
+++ R/zzz.R
@@ -0,0 +1,22 @@
+#' @importFrom utils globalVariables
+
+globalVariables(
+  c(
+    'Score',
+    'Portions',
+    'Nutrients',
+    'AHEI_ComponentIndex',
+    'GROUP',
+    'SERVINGS',
+    'FIBRE',
+    'EICOSAPENTAENOIC_ACID',
+    'DOCOSAHEXAENOIC_ACID',
+    'POLYUNSATURATED_FAT',
+    'Value',
+    'KCALS',
+    'PUFA',
+    'Energy',
+    'SODIUM',
+    'TRANS_FAT'
+  )
+)
