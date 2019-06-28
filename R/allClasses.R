6cd0ddb23d50e530807ff24fc49beda2453a5a95
wilsontom <wilsontom89@gmail.com>
2019-06-28 12:34:09 +0100

    new simpler class

--- R/allClasses.R
+++ R/allClasses.R
@@ -1,21 +1,10 @@
 #' FFQ Class
 #'
 #' @slot FFQ
-#' @slot Portions
 #' @slot Nutrients
-#' @slot Components
+#' @export
 #'
 
-
-setClass(
-  'FFQ',
-  slots = list (
-    FFQ = 'tbl_df',
-    Portions = 'tbl_df',
-    Nutrients = 'tbl_df',
-    Components = 'tbl_df'
-
-  )
-)
-
-
+setClass('ffqr',
+         slots = list (FFQ = 'list',
+                       Nutrients = 'list'))
