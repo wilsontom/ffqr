c64bda105e18cb6d50594c299b81f2be7f40bc1b
wilsontom <wilsontom89@gmail.com>
2019-07-15 13:52:38 +0100

    add S4 class for storing AHEI results

--- R/allClasses.R
+++ R/allClasses.R
@@ -10,3 +10,22 @@
 setClass('ffqr',
          slots = list (FFQ = 'list',
                        Nutrients = 'list'))
+
+
+
+#' AHEI2010 Class
+#'
+#' A S4 class to store the scores from the AHEI-2010 calculation
+#'
+#' @slot rawValues
+#' @slot componentScores
+#' @slot AHEIScore
+#' @export
+
+
+setClass('AHEI',
+         slots = list(
+           rawValues = 'tbl_df',
+           componentScores = 'tbl_df',
+           AHEIScore = 'numeric'
+         ))
